4ea1a906cb7cda078a64864472c10b15272c1ee9
ushiushi05 <ushin1211@gmail.com>
2023-10-26 21:13:29 +0900

    楽曲の情報を表示する表を作成しました

--- app.py
+++ app.py
@@ -13,26 +13,34 @@ st.title('音楽レコメンド')
 # 楽曲情報の読み込み
 music = pd.read_csv("data/data.csv")
 
+# 楽曲情報データ
 music_data = music.drop('user_id', axis=1)
-music_data = music_data.drop_duplicates().sort_values('song_name')
+music_data = music_data.drop_duplicates().sort_values('song_name').reset_index(drop=True)
+
 # アプリで4種類から選択できるようにする
 song_name = sorted(list(set(music['song_name'].tolist())))
 artist = sorted(list(set(music['artist_name'].tolist())))
 composer = sorted(list(set(music['composer'].tolist())))
 lyricist = sorted(list(set(music['lyricist'].tolist())))
-length = range(1, 11)
+
 # userが聞いた曲のdf
 unique = music.groupby('user_id').agg({'song_name': list})
 listened_list = unique['song_name'].tolist()
 
 
-st.markdown("## 1つの楽曲に対して他のユーザーが聞いた楽曲を表示します")
-selected_music = st.selectbox("楽曲を選んでください", music_data['song_name'])
-selected_length = 3
-howmany_length = st.selectbox("どのくらいの曲数を表示しますか", length)
-st.write(f"あなたが選択した楽曲は{selected_music}です")
-# st.write(f'あなたが選んだ楽曲の情報は{music_data[selected_music]}')
+st.markdown("### 1つの楽曲に対して他のユーザーが聞いた楽曲を表示します")
+selected_music = st.selectbox("楽曲を選んでください", song_name)
+
+howmany_length = st.slider("どのくらいの曲数を表示しますか", 1, 10)
+st.divider()
+music_info = music_data[music_data["song_name"].isin([selected_music])]
+
+st.write(f"あなたが選択した楽曲は{selected_music}です。楽曲の情報は下記のようになります")
+# st.write(f'あなたが選んだ楽曲の情報はこちらです')
+st.table(music_info)
+
 # 趣味が似ているユーザーの聞いた楽曲を表示
+selected_length = 3
 st.markdown(f"### {selected_music}を聞いた人はこの楽曲も聴いています")
 results = []
 for i in range(len(listened_list)):
