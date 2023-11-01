ca8c866516768944ac932f64f8bb340bc61d5c6c
ushiushi05 <ushin1211@gmail.com>
2023-11-01 13:52:41 +0900

    APIうんぬん

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/ushin/first_app/venv"
+set -gx VIRTUAL_ENV "/Users/ushin/music_recommend_app/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
